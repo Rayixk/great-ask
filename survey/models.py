f88995a583e25c01f738e468413f6f80c2d4b610
jackChen8850 <472093743@qq.com>
2017-10-30 20:04:35 +0800

    init

---
+++ survey/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+
+
+class UserInfo(models.Model):
+    username = models.CharField("用户名", max_length=32)
+    password = models.CharField("密码", max_length=64)
+    name = models.CharField("姓名", max_length=32, null=True, blank=True)
+
+    def __str__(self):
+        return self.name if self.name else self.username
+
+
+class Survey(models.Model):
+    title = models.CharField("问卷名称", max_length=128)
+    date = models.DateTimeField("创建日期", auto_created=True)
+
+    def __str__(self):
+        return self.title
+
+
+class ChoiceBox(models.Model):
+    title = models.CharField("问题名称", max_length=128)
+    survey = models.ForeignKey(to="Survey", verbose_name="所属问卷")
+    type_choices = [
+        (1, "单选"),
+        (2, "多选"),
+    ]
+    type = models.SmallIntegerField("选项类型", choices=type_choices, default=1)
+
+    def __str__(self):
+        return self.title
+
+
+class InputBox(models.Model):
+    title = models.CharField("问题名称", max_length=128)
+    answer = models.CharField("答案", max_length=200, null=True, blank=True)
+    survey = models.ForeignKey(to="Survey", verbose_name="所属问卷")
+
+    def __str__(self):
+        return self.title
+
+
+class TextArea(models.Model):
+    title = models.CharField("问题名称", max_length=128)
+    answer = models.TextField("答案", max_length=200, null=True, blank=True)
+    survey = models.ForeignKey(to="Survey", verbose_name="所属问卷")
+
+    def __str__(self):
+        return self.title
+
+
+class Choice(models.Model):
+    title = models.CharField("选项名称", max_length=128)
+    is_checked = models.BooleanField("是否选中", default=False)
+    choice_box = models.ForeignKey(to="ChoiceBox", verbose_name="所属问题")
+
+    def __str__(self):
+        return self.title
