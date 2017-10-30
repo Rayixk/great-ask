15c3ba6c3b97e4d219fed506ffe4f5302143983a
jackChen8850 <472093743@qq.com>
2017-10-31 00:05:40 +0800

    something need

--- survey/arya.py
+++ survey/arya.py
@@ -3,37 +3,39 @@
 from . import models
 from arya.service import sites
 
+
 class UserInfoConfig(sites.AryaConfig):
     list_display = ['name']
-sites.site.register(models.UserInfo,UserInfoConfig)
+
+
+sites.site.register(models.UserInfo, UserInfoConfig)
+
 
 class SurveyConfig(sites.AryaConfig):
     list_display = ['title']
 
-sites.site.register(models.Survey,SurveyConfig)
+
+sites.site.register(models.Survey, SurveyConfig)
+
 
 class ChoiceBoxConfig(sites.AryaConfig):
     list_display = ['title']
-sites.site.register(models.ChoiceBox,ChoiceBoxConfig)
+
+
+sites.site.register(models.ChoiceBox, ChoiceBoxConfig)
+
 
 class InputBoxConfig(sites.AryaConfig):
     list_display = ['title']
-sites.site.register(models.InputBox,InputBoxConfig)
 
-class TextAreaConfig(sites.AryaConfig):
-    list_display = ['title']
-sites.site.register(models.TextArea,TextAreaConfig)
+
+sites.site.register(models.InputBox, InputBoxConfig)
+
 
 class ChoiceConfig(sites.AryaConfig):
-    def choice_box(self,obj=None, is_header=False):
-        if is_header:
-            return "所属问题"
-        return "{}({})".format(obj.choice_box.title,obj.choice_box.survey.title)
 
-    def status(self,obj=None, is_header=False):
-        if is_header:
-            return "是否选中"
-        return obj.get_status_display()
 
-    list_display = [choice_box,"title",status]
-sites.site.register(models.Choice,ChoiceConfig)
+    list_display = ["title"]
+
+
+sites.site.register(models.Choice, ChoiceConfig)
--- survey/models.py
+++ survey/models.py
@@ -3,7 +3,7 @@ from rbac.models import User
 
 
 class UserInfo(models.Model):
-    user = models.OneToOneField(to=User,verbose_name="用户账号")
+    user = models.OneToOneField(to=User, verbose_name="用户账号")
     name = models.CharField("姓名", max_length=32)
 
     def __str__(self):
@@ -13,6 +13,8 @@ class UserInfo(models.Model):
 class Survey(models.Model):
     title = models.CharField("问卷名称", max_length=128)
     date = models.DateField("创建日期", auto_now_add=True)
+    choice_boxes = models.ManyToManyField(to="ChoiceBox", verbose_name="选择题")
+    input_boxes = models.ManyToManyField(to="InputBox", verbose_name="填空题")
 
     def __str__(self):
         return self.title
@@ -20,12 +22,12 @@ class Survey(models.Model):
 
 class ChoiceBox(models.Model):
     title = models.CharField("问题名称", max_length=128)
-    survey = models.ForeignKey(to="Survey", verbose_name="所属问卷")
     type_choices = [
         (1, "单选"),
         (2, "多选"),
     ]
-    type = models.SmallIntegerField("选项类型", choices=type_choices, default=1)
+    type = models.SmallIntegerField("选择题类型", choices=type_choices, default=1)
+    choices = models.ManyToManyField(to="Choice",verbose_name="选项")
 
     def __str__(self):
         return self.title
@@ -33,17 +35,11 @@ class ChoiceBox(models.Model):
 
 class InputBox(models.Model):
     title = models.CharField("问题名称", max_length=128)
-    answer = models.CharField("答案", max_length=200, null=True, blank=True)
-    survey = models.ForeignKey(to="Survey", verbose_name="所属问卷")
-
-    def __str__(self):
-        return self.title
-
-
-class TextArea(models.Model):
-    title = models.CharField("问题名称", max_length=128)
-    answer = models.TextField("答案", max_length=200, null=True, blank=True)
-    survey = models.ForeignKey(to="Survey", verbose_name="所属问卷")
+    type_choices = [
+        (1, "text"),
+        (2, "text-area"),
+    ]
+    type = models.SmallIntegerField("填空题类型", choices=type_choices, default=1)
 
     def __str__(self):
         return self.title
@@ -51,12 +47,22 @@ class TextArea(models.Model):
 
 class Choice(models.Model):
     title = models.CharField("选项名称", max_length=128)
-    status_choices=[
-        (0,"false"),
-        (1,"true"),
-    ]
-    status = models.SmallIntegerField("是否选中",choices=status_choices,default=0)
-    choice_box = models.ForeignKey(to="ChoiceBox", verbose_name="所属问题")
 
     def __str__(self):
         return self.title
+
+
+class InputRecord(models.Model):
+    """填空题记录表"""
+    user = models.ForeignKey(to="UserInfo", verbose_name="用户")
+    survey = models.ForeignKey(to="Survey", verbose_name="问卷")
+    question = models.ForeignKey(to="InputBox", verbose_name="问题")
+    answer = models.CharField("答案", max_length=512)
+
+
+class ChoiceRecord(models.Model):
+    """选择题记录表"""
+    user = models.ForeignKey(to="UserInfo", verbose_name="用户")
+    survey = models.ForeignKey(to="Survey", verbose_name="问卷")
+    question = models.ForeignKey(to="ChoiceBox", verbose_name="问题")
+    answer = models.ForeignKey(to="Choice")
